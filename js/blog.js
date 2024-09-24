ccc826b9631ca7f8a48a626efca7a5c442fbddcb
Orinakter <orin.akter9013@gmail.com>
2024-09-24 13:04:20 +0600

    blog functionality and card1 functionality done

---
+++ js/blog.js
@@ -0,0 +1,3 @@
+document.getElementById("ButtonHome").addEventListener("click", function(){
+    window.location.href = "./index.html"
+});
---
+++ js/card1.js
@@ -0,0 +1,53 @@
+
+document.getElementById("DonateButton1").addEventListener("click", function(){
+
+    let input1 = allInputValue("input1");
+    let Balance = textData("balance")
+    let cardAmount1 = textData("card-amount1");
+
+    if(Balance>input1 || !isNaN(input1) || input1>0){
+        document.getElementById("DonateButton1").setAttribute("onclick", "my_modal_5.showModal()")
+    }
+    else{
+        document.getElementById("DonateButton1").removeAttribute("onclick", "my_modal_5.showModal()")
+    }
+    if(Balance<input1 || isNaN(input1) ||  input1<0){
+        alert("Input value Wrong input Positive number");
+        
+        return ;
+    }
+    else{
+        
+       const totalDonation = cardAmount1 + input1;
+       const remainingBalance = Balance-input1;
+   document.getElementById("balance").innerText = remainingBalance;
+   document.getElementById("card-amount1").innerText = totalDonation;
+
+   let historySection = document.getElementById("history");
+   let card1h2 = document.getElementById("card1-h2").innerText;
+   
+   const date = new Date;
+   const day = date.toDateString().split(' ')[0];
+   const month = date.toDateString().split(' ')[1];
+   const dayNum = date.getDate();
+   const year = date.getFullYear();
+   const time = date.toTimeString().split(' ')[0]; 
+   const timezone = date.toTimeString().split(' ')[1].replace(/[()]/g, '');
+
+   
+   let div = document.createElement("div");
+   div.classList.add("border-2");
+   div.classList.add("rounded-xl");
+   div.classList.add("p-4");
+
+   div.innerHTML = `
+        <p class="text-2xl font-medium mb-4">${input1} Taka is ${card1h2} </p>
+        <p class="">Date : ${day} ${month} ${dayNum} ${year} ${time} ${timezone}</p>
+        
+        `
+        historySection.appendChild(div);
+
+    }
+
+
+});
